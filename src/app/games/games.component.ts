22b71287d2560485abb5d190a014fa0d4b151bcd
Patrick <molz@in.tum.de>
2020-09-25 13:29:03 +0200

    Add UI

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { isDevMode } from '@angular/core';
+// import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-games',
@@ -9,7 +11,36 @@ export class GamesComponent implements OnInit {
 
   constructor() { }
 
+  debugMessage: String = "";
+  devMode = isDevMode();
+  selectedGame : String = "quiz";
+  gameJoined : boolean = false
+  @Input() sessionId: String;
+  @Input() username: String;
+
   ngOnInit(): void {
   }
+  
+
+  public onJoinGameButton() {
+    this.debugMessage = "Joining Game as " + this.username + " in Session " + this.sessionId + this.selectedGame;
+    this.gameJoined = true;
+    // Disconnect after 2 seconds
+    setTimeout( () => {
+      this.onDisconnect();
+      this.debugMessage = "Disconnected after Timeout"
+    }, 2000);
+
+  }
+
+  isSelected(game){
+    return game == this.selectedGame;
+  }
+
+  onDisconnect() {
+    this.gameJoined = false;
+    this.debugMessage = "Disconnected";
+  }
+
 
 }
