fcfb09bf15426c2bc4e57f170063f80fdd69345d
Michaela Finger <m.finger@mail.de>
2020-09-29 15:43:52 +0200

    integrate angular material and init component for teacher (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { GamesComponent } from './games/games.component';
 import { MockVideoComponent } from './mock-video/mock-video.component';
 import { QuizComponent } from './games/quiz/quiz.component';
 import { AliasComponent } from './games/alias/alias.component';
+import { SettingsComponent } from './teacher/settings/settings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,12 +17,15 @@ import { AliasComponent } from './games/alias/alias.component';
     GamesComponent,
     MockVideoComponent,
     QuizComponent,
-    AliasComponent
+    AliasComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
